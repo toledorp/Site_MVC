3da240612f9096b51761f594acb2a92feb175ce4
toledorp <roge.pupo.toledo@gmail.com>
2025-09-27 18:28:13 -0300

    ajuste imagens e tabela

--- controllers/ContatosController.js
+++ controllers/ContatosController.js
@@ -4,14 +4,46 @@ const router = express.Router()
 
 router.get("/contatos", (req, res) => {
   const contatos = [
-    { nome: "Ana Souza",        email: "ana.souza@ai.com",      ramal: 1010 },
-    { nome: "Bruno Lima",       email: "bruno.lima@ai.com",     ramal: 1020 },
-    { nome: "Carla Mendes",     email: "Carla.Mendes@ai.com",   ramal: 1030 },
-    { nome: "Daniel Oliveira",  email: "daniel.oliveira@ai.com", ramal: 1040 },
-    { nome: "Fernanda Costa",   email: "fernanda.costa@ai.com", ramal: 1050 },
-    { nome: "Gustavo Pereira",  email: "gustavo.pereira@ai.com", ramal: 1060 },
-    { nome: "Helena Rocha",     email: "helena.rocha@ai.com",   ramal: 1070 },
-    { nome: "Igor Santos",      email: "igor.santos@ai.com",    ramal: 1080 },
+    { nome: "Ana Souza",        
+      email: "ana.souza@ai.com",
+      ramal: 1010, 
+      foto: "/img/pessoal/AnaSouza.png"
+    },
+    { nome: "Bruno Lima",
+      email: "bruno.lima@ai.com",
+      ramal: 1020,
+      foto: "/img/pessoal/BrunoLima.png"
+    },
+    { nome: "Carla Mendes",
+      email: "Carla.Mendes@ai.com",
+      ramal: 1030,
+      foto: "/img/pessoal/CarlaMendes.png"
+    },
+    { nome: "Daniel Oliveira",
+      email: "daniel.oliveira@ai.com",
+      ramal: 1040,
+      foto: "/img/pessoal/DanielOliveira.png"
+    },
+    { nome: "Fernanda Costa",
+      email: "fernanda.costa@ai.com",
+      ramal: 1050,
+      foto: "/img/pessoal/FernandaCosta.png"
+    },
+    { nome: "Gustavo Pereira",
+      email: "gustavo.pereira@ai.com",
+      ramal: 1060,
+      foto: "/img/pessoal/GustavoPereira.png"
+    },
+    { nome: "Helena Rocha",
+      email: "helena.rocha@ai.com",
+      ramal: 1070,
+      foto: "/img/pessoal/HelenaRocha.png"
+    },
+    { nome: "Igor Santos",
+      email: "igor.santos@ai.com",
+      ramal: 1080,
+      foto: "/img/pessoal/IgorSantos.png"
+    },    
   ];
   res.render("contatos", {
     contatos : contatos
